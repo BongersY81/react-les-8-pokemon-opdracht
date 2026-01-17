969ab72a4c07a65f72258c0c80059b799b1ff3dc
yvonnebongers <yvonnebongers@gmail.com>
2026-01-17 20:07:12 +0100

    opdracht 1 begonnen

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import './App.css'
+import './App.css';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [pokemon, setPokemon] = useState(null)
+async function getPokemonInformation() {
 
-  return (
+    try {
+        const result = await
+            axios.get("https://pokeapi.co/api/v2/pokemon/",)
+        console.log(result.data);
+        setPokemon(result.data);
+    } catch  (error) {
+        console.error(error);
+    }
+
+}
+useEffect(() => {
+   void getPokemonInformation();
+    }, []);
+
+    return (
     <>
+
       <h1>Gotta catch em all!</h1>
+
     </>
   )
 }
